7c554857a36383ec6b45b8c6d6c1f4e73ceed735
mriyadd22 <mriyadd22@gmail.com>
2026-09-19 00:10:14 +0600

    py project

--- rock_paper_scissor.py
+++ rock_paper_scissor.py
@@ -10,7 +10,6 @@
 
 #===========================================================
 
-
 import random
 
 
@@ -43,3 +42,67 @@ while True:
     should_continue = input("Do you want to play again? (y/n): ").lower()
     if should_continue == "n":
         break
+
+
+
+#=========================================================================================
+"""
+Refactoring: Modularizing Code
+
+Modularization means Breaking down a large program into 
+smaller reusable parts called modules or functions.
+
+"""
+
+
+import random
+
+ROCK = "r"
+PAPER = "p"
+SCISSOR = "s"
+emojis = {ROCK:"🪨", PAPER:"📜", SCISSOR:"✂️"}
+choices = tuple(emojis.keys())
+
+def get_user_choice():
+    while True:
+        user_choice = input("Rock, paper, or scissor? (r/p/s): ").lower()
+        if user_choice in choices:
+            return user_choice
+        else:
+            print("Invalid choice!")
+
+
+def display_choice(user_choice, computer_choice):
+    print(f"You chose {emojis[user_choice]}")
+    print(f"Computer chose {emojis[computer_choice]}")
+
+
+def determine_winner(user_choice, computer_choice):
+    if user_choice == computer_choice:
+        print("Tie!")
+    elif (
+        (user_choice == ROCK and computer_choice == SCISSOR) or
+        (user_choice == SCISSOR and computer_choice == PAPER) or
+        (user_choice == PAPER and computer_choice == ROCK)):
+        print("You win")
+
+    else:
+        print("You lose")
+
+
+def play_game():
+    while True:
+        user_choice = get_user_choice()
+
+        computer_choice = random.choice(choices)
+
+        display_choice(user_choice, computer_choice)
+
+        determine_winner(user_choice, computer_choice)
+
+
+        should_continue = input("Do you want to play again? (y/n): ").lower()
+        if should_continue == "n":
+            break
+
+play_game()
